8dba208caa4bfba95e16a1c16c051d383f2aa7ab
Frankie Barrios <softwaredevfb@gmail.com>
2018-10-15 08:02:56 -0500

    Add files via upload

--- TheGreatRaceMidterm/main.js
+++ TheGreatRaceMidterm/main.js
@@ -5,11 +5,9 @@ Purose: Implement a page that simulates
 a race between two or more competing players.
 */
 
-//Event Listener for Stoplight
-/* document.getElementById("Light").addEventListener("click", startRace); //need to write function still for moving images
- */
-//Global Variables -- Will be used im multiple functions
-var raceSpeed; 
+//Global Variables -- Will be used in multiple functions
+var raceSpeed;
+
 //Each of the vars grab each racers div
 var cullenDiv = document.getElementById("cDiv");
 var gregDiv = document.getElementById("gDiv");
@@ -28,6 +26,7 @@ var gImage = document.getElementById("Greg");
 var fImage = document.getElementById("Frankie");
 var pImage = document.getElementById("Phillip");
 
+//Triggers onLoad and sets up race
 function getReady() {
   //Players current positions
   cPos = 0;
@@ -35,7 +34,7 @@ function getReady() {
   fPos = 0;
   pPos = 0;
 
-  //Places palyers at start line
+  //Places players at start line
   cullenDiv.style.left = cPos + "px";
   gregDiv.style.left = gPos + "px";
   frankieDiv.style.left = fPos + "px";
@@ -60,25 +59,23 @@ function getReady() {
   document.getElementById("PhillipWins").style.display = "none";
 
   //Makes Light image start button
-  document.getElementById("start").style.display = "block";
-  document.getElementById("start").style.width = "250px";
-  document.getElementById("start").style.padding = "25px";
-
-  //document.getElementById("start").style.height = "auto";
+  document.getElementById("start").style.display = "inline-block";
+  document.getElementById("start").style.width = "95px";
+  document.getElementById("start").style.height = "110px";
 
   //Allows me to make the winner img the new start button and light unclickable/ hidden
   document.getElementById("start2").style.display = "none";
-  //raceSpeed = setInterval(movement, 25); //Runs movement() as fast as interval time set in milliseconds
 }
 
 function startRace() {
   getReady();
-  document.getElementById("start").style.display = "none";
-  document.getElementById("start2").style.width = "250px"; //Once initial light clicked, this one appears
+  document.getElementById("start").style.display = "none"; //Hides red light so green light can display
+  document.getElementById("start2").style.width = "95px"; //Once initial light clicked, this one appears
   document.getElementById("start2").style.display = "block"; //Once initial light clicked, this one appears
+  document.getElementById("start2").style.height = "110px";
 
   //Speeds up or slows down racers. Controls movement function.
-  raceSpeed = setInterval(movement, 25); //Runs movement() as fast as interval time set in milliseconds
+  raceSpeed = setInterval(movement, 75); //Runs movement() as fast as interval time set in milliseconds
 }
 
 function movement() {
@@ -99,7 +96,7 @@ function movement() {
   frankieDiv.style.left = fPos + "px";
   phillipDiv.style.left = pPos + "px";
 
-  //Setting Finish Line
+  //Setting Finish Line -- Gets Racers to stop by clearing interval
   if (cPos > 1300 || gPos > 1300 || fPos > 1300 || pPos > 1300) {
     //Handles how far racers will go and finds winner
     clearInterval(raceSpeed); //Once players hit value set, stops running movement function
@@ -126,21 +123,54 @@ function winner() {
 
 // Makes the winning images visible & hides race images
 function cullenWins() {
-  cImage.style.display = "none"; //Hides player 1 race img
+  cImage.style.display = "none";
+  gImage.style.display = "none";
+  fImage.style.display = "none";
+  pImage.style.display = "none";
   document.getElementById("CullenWins").style.display = "block";
+  document.getElementById("CullenWins").style.width = "175px";
+  document.getElementById("CullenWins").style.height = "auto";
+  document.getElementById("CullenWins").style.position = "fixed";
+  document.getElementById("CullenWins").style.top = "40%";
+  document.getElementById("CullenWins").style.left = "45%";
 } //end of cullenWins()
 
 function gregWins() {
-  gImage.style.display = "none"; //Hides player 1 race img
+  gImage.style.display = "none";
+  fImage.style.display = "none";
+  pImage.style.display = "none";
+  cImage.style.display = "none";
   document.getElementById("GregWins").style.display = "block";
+  document.getElementById("GregWins").style.width = "175px";
+  document.getElementById("GregWins").style.height = "auto";
+  document.getElementById("GregWins").style.position = "fixed";
+  document.getElementById("GregWins").style.top = "40%";
+  document.getElementById("GregWins").style.left = "45%";
 } //end of gregWins()
 
 function frankieWins() {
-  fImage.style.display = "none"; //Hides player 1 race img
+  fImage.style.display = "none";
+  pImage.style.display = "none";
+  cImage.style.display = "none";
+  gImage.style.display = "none";
   document.getElementById("FrankieWins").style.display = "block";
+  document.getElementById("FrankieWins").style.width = "175px";
+  document.getElementById("FrankieWins").style.height = "auto";
+  document.getElementById("FrankieWins").style.position = "fixed";
+  document.getElementById("FrankieWins").style.top = "40%";
+  document.getElementById("FrankieWins").style.left = "45%";
 } //end of frankieWins()
 
 function phillipWins() {
-  pImage.style.display = "none"; //Hides player 1 race img
+  pImage.style.display = "none";
+  cImage.style.display = "none";
+  gImage.style.display = "none";
+  fImage.style.display = "none";
   document.getElementById("PhillipWins").style.display = "block";
+  document.getElementById("PhillipWins").style.width = "175px";
+  document.getElementById("PhillipWins").style.height = "auto";
+  document.getElementById("PhillipWins").style.position = "fixed";
+  document.getElementById("PhillipWins").style.top = "40%";
+  document.getElementById("PhillipWins").style.left = "45%";
+  document.getElementsByTagName("body").style.backgroundImage; url("Graduation.jpg");
 } //end of PhillipWins()
